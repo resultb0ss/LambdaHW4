a1142fd76284d09389e8806b660284669ca5bbf1
Admin <reteamboss@gmail.com>
2024-09-03 21:33:34 +0300

    Задание 4

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,16 @@
 fun main() {
-    println("Hello World!")
+    val array = intArrayOf(2,23,43,54,6,456,675,35,243)
+
+    val operation = { array: IntArray ->
+        for (i in array.indices){
+            if (i % 2 == 0){
+                array[i] = array[i] / 2
+            }else{
+                array[i] = array[i] * 3
+            }
+        }
+        array
+    }
+
+    println(operation(array).contentToString())
 }
